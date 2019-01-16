13dcc004b96394fed06d3dac0ff45140a2c4ce15
antlml <kqqlmlant@163.com>
2019-01-16 16:45:45 +0800

    修改

--- src/main/java/com/schoolnews/controller/HomeController.java
+++ src/main/java/com/schoolnews/controller/HomeController.java
@@ -14,9 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 @Controller
 public class HomeController {
     @Autowired
